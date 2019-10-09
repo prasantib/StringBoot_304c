34810850c186888ad279a35c933f2fc7085a946a
prasantib <prasanti.babu@gmail.com>
2019-10-09 16:59:07 -0400

    StringBoot_304

---
+++ src/main/java/com/example/demo/StringBoot304bApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot304bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot304bApplication.class, args);
+    }
+
+}
